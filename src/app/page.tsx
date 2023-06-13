4b517d241249d1a78befaa83e1785e5e8083c403
rivera00255 <rivera00255@gmail.com>
2023-06-13 18:54:06 +0900

    Fix: add revalidate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import Link from 'next/link';
 const libraryApiKey = process.env.NEXT_PUBLIC_LIBRARY_API_KEY;
 const libraryUrl = process.env.NEXT_PUBLIC_LIBRARY_URL;
 
+export const revalidate = 0;
+
 export default async function Home() {
   const data: LibraryRes = await fetcher(
     'GET',
@@ -12,7 +14,7 @@ export default async function Home() {
     undefined,
     'no-store'
   );
-  const bookRentBest = data.SeoulLibraryBookRentNumInfo?.row;
+  // const bookRentBest = data.SeoulLibraryBookRentNumInfo?.row;
 
   return (
     <main>
@@ -26,7 +28,7 @@ export default async function Home() {
       <section className="container xl mx-auto px-4">
         <h3 className="text-xl">인기 대출도서</h3>
         <h4 className="mb-8 text-xs">서울 도서관 인기대출 도서 목록</h4>
-        {bookRentBest?.map((item) => (
+        {data.SeoulLibraryBookRentNumInfo?.row.map((item) => (
           <div key={item.ISBN} className="bg-amber-50 mb-2 rounded px-8 py-4 drop-shadow-sm">
             <p>
               <strong>{item.TITLE}</strong>
